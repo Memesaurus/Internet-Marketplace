a9001832602d489e3c36153e3a8e78d8732928a8
Memesaurus <Greggrr1702@gmail.com>
2023-03-14 22:24:10 +0300

    init

---
+++ src/main/java/com/diploma/gazon/GazonApplication.java
@@ -0,0 +1,13 @@
+package com.diploma.gazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diploma/gazon/GazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diploma.gazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
