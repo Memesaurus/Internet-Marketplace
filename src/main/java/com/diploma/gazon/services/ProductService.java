34f71eeb8f7cceb837991143a4b52f6d6de6f7f8
Memesaurus <Greggrr1702@gmail.com>
2023-03-16 23:05:44 +0300

    fix

---
+++ src/main/java/com/diploma/gazon/services/ProductService.java
@@ -0,0 +1,11 @@
+package com.diploma.gazon.services;
+
+import com.diploma.gazon.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+}
