33dd7429b4cb9a3949600b644e225111b953e4e5
Memesaurus <Greggrr1702@gmail.com>
2023-03-31 23:45:22 +0300

    init frontend api logic

---
+++ gazon-frontend/src/api/apiTypes.ts
@@ -0,0 +1,103 @@
+interface UserAuthRequest {
+    username: string;
+    password: string;
+}
+
+export interface UserLoginRequest extends UserAuthRequest {
+    rememberMe: boolean;
+}
+
+interface UserRegisterRequest extends UserAuthRequest{
+    email: string;
+    additionaInfo: UserAdditionalInfo;
+    role: UserRole;
+    name: string;
+}
+
+export interface CompanyUserRegisterRequest extends UserRegisterRequest {
+    description: string;
+}
+
+export interface MemberUserRegisterRequest extends UserLoginRequest {
+    middleName: string;
+    surName: string;
+    age: number;
+}
+
+export enum UserRole {
+    ADMIN,
+    MEMBER,
+    COMPANY
+}
+
+export interface OrderRequest {
+    products: Map<string, number>;
+    address: UserAddress;
+}
+
+export interface ProductRequest {
+    name: string;
+    description: string;
+    price: number;
+    tags: string[];
+}
+
+export interface ReviewRequest {
+    rating: number;
+    body: string;
+}
+
+export interface User {
+    username: string;
+    email: string;
+    additionalInfo: UserAdditionalInfo;
+}
+
+export interface UserAdditionalInfo {
+    address: UserAddress;
+    phone: string;
+}
+
+export interface UserAddress {
+    country: string;
+    city: string;
+    street: string;
+    house: string;
+}
+
+export interface Order {
+    id: string;
+    createdAt: Date;
+    cancellableUntil: Date;
+    deliveryAddress: string;
+    price: number;
+    user: User;
+    productOrders: ProductOrder[];
+    status: boolean;
+}
+
+export interface ProductOrder {
+    product: Product;
+    quantity: number;
+}
+
+export interface Product {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    isInStock: boolean;
+    rating: number;
+    user: User;
+    images: string[];
+    tags: string[];
+    reviews: Review[];
+}
+
+export interface Review {
+    id: string;
+    createdAt: Date;
+    rating: number;
+    body: string;
+    user: User;
+}
---
+++ gazon-frontend/src/api/axiosConfiguration.ts
@@ -0,0 +1,65 @@
+import axios, { AxiosError } from "axios";
+
+const baseURL = "http://localhost:8080/api";
+
+const api = axios.create({
+    baseURL: baseURL,
+    withCredentials: true
+});
+
+const utilApi = axios.create({
+    baseURL: baseURL,
+    withCredentials: true
+})
+
+
+api.interceptors.request.use(async config => {
+    if (!isCookiePresent("XSRF-TOKEN")) {
+        console.log('xsrf token not present');
+        
+        await utilApi.get("/auth/csrf")
+    }
+
+    const token = getCsrfCookie("XSRF-TOKEN");
+    
+    api.defaults.headers.common['X-XSRF-TOKEN'] = token;
+
+    return config;
+});
+
+const isCookiePresent = (key: string): boolean => {
+    return Boolean(getRegex(key));
+}
+
+const getCsrfCookie = (key: string): string | undefined => {
+    const ans = getRegex(key);
+
+    return ans ? ans.pop() : undefined;
+}
+
+const getRegex = (key: string): RegExpMatchArray | null => document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
+
+api.interceptors.response.use(
+    response => response, 
+    async error => {
+        const lastRequest = error.config;
+        
+        if (isNotJWTExpiredError(error) || lastRequest._retried) {                       
+            return error;
+        }
+    
+        try {
+            await utilApi.post("/auth/refresh")
+        } catch (e) {            
+            return e;
+        }
+
+        return api(lastRequest);
+    }
+)
+
+const isNotJWTExpiredError = (error: AxiosError): boolean => {
+    return error.status != 401 && !error.request?.response.includes("JWT expired");
+}
+
+export default api;
